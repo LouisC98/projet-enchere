1d25024df542610878997364f039343b435f2f8a
LouisC98 <louis.carvalho98@gmail.com>
2025-04-23 11:15:38 +0200

    ajout de l'index et la recherche dans les routes autorisé a tous

--- src/main/java/fr/eni/encheres/config/SecurityConfig.java
+++ src/main/java/fr/eni/encheres/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/login", "/register", "/change-password", "/css/**", "/js/**", "/images/**").permitAll()
+                    .requestMatchers("/","/articles/search","/login", "/register", "/change-password", "/css/**", "/js/**", "/images/**").permitAll()
                     .anyRequest().authenticated())
             .formLogin(form -> form
                     .loginPage("/login")
