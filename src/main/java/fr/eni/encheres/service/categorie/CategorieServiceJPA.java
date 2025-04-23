43e64c0bd763ce2f400f37a6bf59923641a70a57
LouisC98 <louis.carvalho98@gmail.com>
2025-04-23 16:35:52 +0200

    JPA | ajout mockbdd categories

--- src/main/java/fr/eni/encheres/service/categorie/CategorieServiceJPA.java
+++ src/main/java/fr/eni/encheres/service/categorie/CategorieServiceJPA.java
@@ -1,6 +1,8 @@
 package fr.eni.encheres.service.categorie;
 
 import fr.eni.encheres.bo.Categorie;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
@@ -11,11 +13,21 @@ import java.util.List;
 @Repository
 public class CategorieServiceJPA implements CategorieService {
 
+    @Autowired
     private CategorieRepository categorieRepository;
 
+    @PostConstruct
+    private void mockBdd() {
+        if (categorieRepository.count() == 0) {
+            Categorie c1 = new Categorie();
+            c1.setLibelle("Maison");
 
-    public CategorieServiceJPA(CategorieRepository categorieRepository) {
-        this.categorieRepository = categorieRepository;
+            Categorie c2 = new Categorie();
+            c2.setLibelle("Informatique");
+
+            categorieRepository.save(c1);
+            categorieRepository.save(c2);
+        }
     }
 
     @Override
